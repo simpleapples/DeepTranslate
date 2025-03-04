6d58e86a731356b00f0c2229f60312dfc04e801e
simpleapples <zangzhiya@gmail.com>
2025-03-04 21:19:29 +0000

    Initial Commit

---
+++ DeepTranslate/DeepTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepTranslateApp.swift
+//  DeepTranslate
+//
+//  Created by Zzy on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeepTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
